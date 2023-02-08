4c76f28904457e2129e3deaa2cd5bfe179016bac
Katarzyna KLEPACKA <kklepacka@nx-domain.lan>
2023-02-08 15:28:42 +0100

    commit

--- src/test/java/testcscu4/StepDefinition.java
+++ src/test/java/testcscu4/StepDefinition.java
@@ -1,122 +1,109 @@
 package testcscu4;
 
-import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 
 public class StepDefinition {
-    @Given("test '")
-    public void test1() {
-        System.out.println("Test");
-    }
-    @Given("Test ` echo test `")
-public void test2() {
-    System.out.println("Test");
-}
 
-    @Given("Test $test")
-    public void test3() {
-        System.out.println("Test");
+    @When("test '")
+    public void test1(String docString) {
+
+        System.out.println("test");
     }
 
-    @Given("Test & echo toto")
-    public void test4() {
-        System.out.println("Test");
+    @When("Test ` echo test `")
+    public void test2(String docString) {
+
+        System.out.println("test");
     }
 
-    @Given("Test ; echo toto")
-    public void test5() {
-        System.out.println("Test");
+    @When("Test $test")
+    public void test3(String docString) {
+
+        System.out.println("test");
     }
 
-    @Given("Test | echo toto")
-    public void test6() {
-        System.out.println("Test");
+    @When("Test & echo toto")
+    public void test4(String docString) {
+
+        System.out.println("test");
     }
 
-    // @Given("Test echo test < /tmp")
-    // public void test7() {
-    //     System.out.println("Test");
-    // }
+    @When("Test ; echo toto")
+    public void test5(String docString) {
 
-    // @Given("Test echo test > /tmp")
-    // public void test8() {
-    //     System.out.println("Test");
-    // }
-
-    @Given("Test ^")
-    public void test9() {
-        System.out.println("Test");
-    }
-    @Given("Test %test%")
-    public void test10() {
-        System.out.println("Test");
+        System.out.println("test");
     }
 
-    @Given("Test $\\{echo test}")
-    public void test11() {
-        System.out.println("Test");
+    @When("Test | echo toto")
+    public void test6(String docString) {
+
+        System.out.println("test");
     }
 
-    // @Given("Test backslash \\\\")
-    // public void test12() {
-    //     System.out.println("Test");
-    // }
+    @When("Test ^")
+    public void test7(String docString) {
 
-    // @Given("Test slash /")
-    // public void test13() {
-    //     System.out.println("Test");
-    // }
-
-    @Given("Test $\\(echo test)")
-    public void test14() {
-        System.out.println("Test");
+        System.out.println("test");
     }
 
-    @Given("Test #test")
-    public void test15() {
-        System.out.println("Test");
+    @When("Test %test%")
+    public void test8(String docString) {
+
+        System.out.println("test");
     }
 
-    @Given("Test 𨱏")
-    public void test16() {
-        System.out.println("Test");
+    @When("Test $\\{echo test}")
+    public void test9(String docString) {
+
+        System.out.println("test");
     }
 
-    @Given("Test 👩‍❤️‍💋‍👨")
-    public void test17() {
-        System.out.println("Test");
+    @When("Test $\\(echo test)")
+    public void test10(String docString) {
+
+        System.out.println("test");
     }
 
-    @Given("Test [test]")
-    public void test18() {
-        System.out.println("Test");
+    @When("Test #test")
+    public void test11(String docString) {
+
+        System.out.println("test");
     }
 
-    @Given("Test ^^")
-    public void test19() {
-        System.out.println("Test");
+    @When("Test 𨱏")
+    public void test12(String docString) {
+
+        System.out.println("test");
     }
 
-    // @Given("Test "test""")
-    // public void test20(String string) {
-    //     System.out.println("Test");
-    // }
-    // @Given("Test 'test'")
-    // public void test21(String string) {
-    //     System.out.println("Test");
-    // }
+    @When("Test 👩‍❤️‍💋‍👨")
+    public void test13(String docString) {
 
-    // @Given("Test $test")
-    // public void test22() {
-    //     System.out.println("Test");
-    // }
+        System.out.println("test");
+    }
 
-    @Given("Test ,.+-_~@€çé=àû*?!")
-    public void test23() {
-        System.out.println("Test");
+    @When("Test [test]")
+    public void test14(String docString) {
+
+        System.out.println("test");
     }
-    @Given("Test .\\/test")
-    public void test24() {
-        System.out.println("Test");
+
+    @When("Test ^^")
+    public void test15(String docString) {
+
+        System.out.println("test");
     }
-    
+
+    @When("Test ,.+-_~@€çé=àû*?!")
+    public void test16(String docString) {
+
+        System.out.println("test");
+    }
+
+    @When("Test .\\/test")
+    public void test17(String docString) {
+
+        System.out.println("test");
+
+    }
 }
